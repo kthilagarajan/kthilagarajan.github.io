be7e1c08e8c76da21426f3b31d1922dc2b0b7010
Thilak <thilagarajan.kamarajan@tpfsoftware.com>
2016-12-26 17:43:08 +0530

    About Me Updated

--- scripts/controllers/about.js
+++ scripts/controllers/about.js
@@ -1,6 +1,28 @@
 angular.module('demoApp')
     .controller('AbtCtrl', ['$scope',
 	function($scope){
+		
+		var highlights = ["Born","10th Grade","High School","Under Graduate","Post Graduate"]
+		var highlightDate = ["11-July-1994","April-2009","March-2011","May-2015","In Progress"]
+		var highlightLocation = ["Hometown, Chennai","B.C.S Jain Matriculation Hr. Sec. School, Royapuram, Ch-21","K.C.Sankaralinga Nadar Hr. Sec. School, Old Washermenpet, Ch-21","Jerusalem College of Engineering, Affiliated to Anna University, Ch-100","Work Integrated Learning Program, BITS Pilani, Rajasthan"]
+		var highlightClass = ["","timeline-inverted","timeline-inverted","",""]
+		var highlightColor = ["info","info","info","info","info"]
+		var highlightIcon = ["fa-home","fa-book","fa-book","fa-bookmark","fa-bookmark"]
+		
+		
+		var detailInfo = []
+		for(var hl in highlights){
+			detailInfo.push({
+				"info": highlights[hl],
+				"date" : highlightDate[hl],
+				"location" : highlightLocation[hl],
+				"class" : highlightClass[hl],
+				"color" : highlightColor[hl],
+				"icon" : highlightIcon[hl]
+			})
+		}
+		
+		$scope.durationData = detailInfo
 		console.log("Abt Controler")
 			
 	}]);
