3422823a1d9b2ea7c3156d0918246290c2e944b7
Thilak <thilagarajan.kamarajan@tpfsoftware.com>
2018-03-28 19:45:08 +0530

    Mobile Check

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
-if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-    document.getElementById("aboutcard").removeAttribute("data-uk-sortable")
-}
+setTimeout(function() {
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        document.getElementById("aboutcard").removeAttribute("data-uk-sortable")
+    }
+}, 1000)
