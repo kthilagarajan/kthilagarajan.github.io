d3fd990c9c6b1695f8149e451f897ab2f3319fb5
Thilak <thilagarajan.kamarajan@tpfsoftware.com>
2018-03-28 22:31:49 +0530

    Share Added

--- scripts.js
+++ scripts.js
@@ -3,3 +3,16 @@ setTimeout(function() {
         document.getElementById("aboutcard").removeAttribute("data-uk-sortable")
     }
 }, 1000)
+
+function doShare() {
+    console.log("Share")
+    if (navigator.share) {
+        navigator.share({
+                title: 'Thilagarajan Kamarajan',
+                text: 'About me',
+                url: 'https://kthilagarajan.github.io',
+            })
+            .then(() => console.log('Successful share'))
+            .catch((error) => console.log('Error sharing', error));
+    }
+}
